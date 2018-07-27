dd8a6093f8a14a00d5f324c3b40cc7a680c5e7d8
Benjaseg96 <benjamin.s1.e@gmail.com>
2018-07-26 20:36:01 -0400

    Close result en modal

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -219,9 +219,9 @@ export class HeaderComponent implements OnInit {
   open(content) {
 
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
+      
     }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      
     });
   }
 
