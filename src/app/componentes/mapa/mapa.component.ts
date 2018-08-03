d20af39efaa7eca363214de4d98d8c402044d2d2
Benjaseg96 <benjamin.s1.e@gmail.com>
2018-08-03 09:43:27 -0400

    Restaurando L.circleMarker

--- src/app/componentes/mapa/mapa.component.ts
+++ src/app/componentes/mapa/mapa.component.ts
@@ -2355,7 +2355,7 @@ calcularAreaPunto(){
 				        return L.marker(latlng, {icon: iconoNuevo.icono});
 					}else{
 
-				        return L.circle(latlng, estilo);
+				        return L.circleMarker(latlng, estilo);
 					}
 			    },
 			    onEachFeature: popup}).addTo(this.activeMap);
