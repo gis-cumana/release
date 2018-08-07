d68c0e93d484e7d5d620b1c8666618ff393be230
Benjaseg96 <benjamin.s1.e@gmail.com>
2018-08-07 09:15:53 -0400

    Equipo desarrollador incorporado en modal

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -10,6 +10,9 @@ export class FooterComponent implements OnInit {
 
 	ventanas: any[];
 
+	investigadores: any[];
+	desarrolladores: any[];
+
 	ventanaActiva: any;
 
 	modalRef: any;
@@ -26,7 +29,46 @@ export class FooterComponent implements OnInit {
   		"Investigadores",
   		"Desarrolladores",
   		"Acerca de..."
-  	]
+  	];
+  	this.investigadores = [
+  		{"nombre": "Dr. Jorge Rojas", "correo": "rogojorge@gmail.com", "telefono": "0416-3200906" "foto": "jorge.jpg"},
+  		{"nombre": "MSc. Francisco Alvarez", "correo": "faovenezuela@gmail.com", "telefono": "0414-8242002" "foto": "francisco.jpeg"},
+  		{"nombre": "MSc. Ysimar Rivera", "correo": "ysymar.river@gmail.com", "telefono": "0416-8936376" "foto": "ysimar.jpg"}
+  	];
+  	this.desarrolladores = [
+  		[{
+  			"nombre": "MSc. Yalgis Rodriguez",
+  			"correo": "yalgisrodriguez@gmail.com",
+  			"telefono": "0416-6143465"
+  			"foto": "yalgis.jpeg"
+  		},{
+  			"nombre": "MSc. Luis M. Rodriguez",
+  			"correo": "luismrodriguezf@gmail.com",
+  			"telefono": "0416-8945712"
+  			"foto": "luism.jpeg"
+  		},{
+  			"nombre": "MSc. Yulia Sardella",
+  			"correo": "sardellayulia@gmail.com",
+  			"telefono": "0416-4813369"
+  			"foto": "yulia.jpg"
+  		}],
+  		[{
+  			"nombre": "MSc. Gregorio Ruiz",
+  			"correo": "gruizleon@gmail.com",
+  			"telefono": "0426-2871235"
+  			"foto": "gregorio.jpg"
+  		},{
+  			"nombre": "TSU. Benjamin Escobar",
+  			"correo": "benjamin.s1.e@gmail.com",
+  			"telefono": "0416-0337683"
+  			"foto": "benjamin.jpg"
+  		},{
+  			"nombre": "Ing. Carlos Cercado",
+  			"correo": "cercadocarlos@gmail.com",
+  			"telefono": "0426-3814727"
+  			"foto": "cercado.png"
+  		}]
+  	];
   }
 
   abrirModal(ventana, content){
