cbed13f612ea7fb04b1a17ec03b86d9729691840
benjalorc <benjalorc@gmail.com>
2018-08-20 10:43:41 -0400

    IconAnchor

--- src/app/componentes/mapa/mapa.component.ts
+++ src/app/componentes/mapa/mapa.component.ts
@@ -2479,7 +2479,7 @@ calcularAreaPunto(){
 					    textColor: opciones.color,
 					    iconSize: [30,30],
 					    iconAnchor: [15,15],
-					    innerIconAnchor: [0, 8],
+					    innerIconAnchor: [4, 12],
 					    innerIconStyle: "font-size: 1rem;",
 					    backgroundColor: "white"
 					};
