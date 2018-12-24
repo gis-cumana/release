2fecbb1c6ab660ce853f26453b1ebd3096926bad
benjalorc <benjalorc@gmail.com>
2018-12-23 22:13:42 -0400

    Rutas de inicio

--- src/app/inicio.module.ts
+++ src/app/inicio.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FlashMessagesModule } from 'angular2-flash-messages/module';
 
@@ -9,11 +9,16 @@ import { HeaderModule } from './componentes/header/header.module';
 import { HomeComponent } from './componentes/home/home.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 
+const inicioRoutes : Routes = [
+ { path: '', component: HomeComponent},
+]
+
 @NgModule({
   imports: [
     CommonModule,
  	FormsModule,
  	FlashMessagesModule,
+ 	RouterModule.forRoot(inicioRoutes),
  	HeaderModule
   ],
   declarations: [HomeComponent, FooterComponent]
